93d346cd06f0cc3e1f7361e7e6e2e00d83999e9e
LukasLen <24789557+LukasLen@users.noreply.github.com>
2018-06-13 20:57:15 +0200

    Bug fixing
    General:
    Fixed a typo
    
    Firefox:
    Fixed that icon was not always showing up
    
    Chrome:
    Fixed versioning

--- firefox/background.js
+++ firefox/background.js
@@ -1,10 +1,19 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({on: true, first: true});
 });
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url && changeInfo.status==='complete' && tab.url.match('web.whatsapp.com')) {
-        chrome.pageAction.show(tabId);
-    } else {
-        chrome.pageAction.hide(tabId);
-    }
+function initializePageAction(tab) {
+  if (tab.url.includes("web.whatsapp.com")) {
+    browser.pageAction.show(tab.id);
+  }
+}
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
+});
+
+//on load
+var gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
 });
