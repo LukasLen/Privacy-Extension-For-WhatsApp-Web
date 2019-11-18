0b23830ab5b6cb3c6f176139f3327cf94fbbb271
LukasLen <24789557+LukasLen@users.noreply.github.com>
2019-11-18 10:21:37 +0100

    Done working on messages

--- firefox/popup/popup.js
+++ firefox/popup/popup.js
@@ -14,7 +14,8 @@ let mainContent = document.getElementById('mainContent');
 let popupMessage = document.getElementById('popupMessage');
 
 // Get and set current version
-document.getElementById('version').innerText = chrome.runtime.getManifest().version;
+let version = chrome.runtime.getManifest().version;
+document.getElementById('version').innerText = version;
 
 // Add or remove stylesheets
 function refreshScript(){
@@ -51,21 +52,27 @@ chrome.storage.sync.get([
       xmlhttp=new XMLHttpRequest();
       xmlhttp.onreadystatechange=function(){
         if (xmlhttp.readyState==4 && xmlhttp.status==200){
-          if(xmlhttp.responseText != "" && data.currentPopupMessage != xmlhttp.responseText){
+          let response = JSON.parse(xmlhttp.responseText);
+          if(response["*"] && response["*"]["min"] <= version && response["*"]["max"] >= version)
+            response = response["*"]["msg"];
+          else
+            response = response[version] ? response[version] : '';
+
+          if(response != "" && data.currentPopupMessage != response){
             mainContent.style.display = "none";
-            popupMessage.innerText = xmlhttp.responseText;
+            popupMessage.innerText = response;
             popupMessage.innerHTML += "<br><a href=\"#\" id=\"popupMessageButton\">Close message</a>";
 
             let popupMessageButton = document.getElementById('popupMessageButton');
             popupMessageButton.addEventListener('click', function() {
-              chrome.storage.sync.set({currentPopupMessage: xmlhttp.responseText});
+              chrome.storage.sync.set({currentPopupMessage: response});
               popupMessage.innerHTML = "";
               mainContent.style.display = "initial";
             });
           }
         }
       }
-      xmlhttp.open("GET", "https://lukaslen.com/message/pfwa.txt", true);
+      xmlhttp.open("GET", "https://lukaslen.com/message/pfwa.json", true);
       xmlhttp.send();
     });
 });
