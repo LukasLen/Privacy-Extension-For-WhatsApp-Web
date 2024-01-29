3960de64537a6199e506d57fe4d3950d3912bf65
Lukas <24789557+LukasLen@users.noreply.github.com>
2024-01-29 14:48:48 +0100

    Migrated to manifest v3

--- src/background.js
+++ src/background.js
@@ -1,56 +1,46 @@
-chrome.runtime.onInstalled.addListener(function(details) {
-  // set storage variables
-  chrome.storage.sync.get([
-      'on',
-      'currentPopupMessage',
-      'messages',
-      'messagesPreview',
-      'mediaPreview',
-      'mediaGallery',
-      'textInput',
-      'profilePic',
-      'name',
-      'noDelay',
-      'unblurActive'
-    ], function(data) {
-      data.on == null && chrome.storage.sync.set({on: true});
-      data.currentPopupMessage == null && chrome.storage.sync.set({currentPopupMessage: ""});
-      data.messages == null && chrome.storage.sync.set({messages: true});
-      data.messagesPreview == null && chrome.storage.sync.set({messagesPreview: true});
-      data.textInput == null && chrome.storage.sync.set({textInput: true});
-      data.mediaPreview == null && chrome.storage.sync.set({mediaPreview: true});
-      data.mediaGallery == null && chrome.storage.sync.set({mediaGallery: true});
-      data.profilePic == null && chrome.storage.sync.set({profilePic: false});
-      data.name == null && chrome.storage.sync.set({name: false});
-      data.noDelay == null && chrome.storage.sync.set({noDelay: false});
-      data.unblurActive == null && chrome.storage.sync.set({unblurActive: false});
+/* Privacy Extension for WhatsApp Web */
+
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get(["settings"]).then((result) => {
+    if (!result.hasOwnProperty("settings")) {
+      chrome.storage.sync.set({
+        settings: {
+          on: true,
+          currentPopupMessage: "",
+          styles: {
+            mediaGallery: true,
+            mediaPreview: true,
+            messages: true,
+            messagesPreview: true,
+            name: false,
+            noDelay: false,
+            profilePic: false,
+            textInput: true,
+            unblurActive: false
+          }
+        },
+      });
+    }
   });
 });
 
-chrome.commands.onCommand.addListener(function(command) {
-  if(command == 'toggle'){
-    chrome.storage.sync.get(['on'], function(data) {
-      chrome.storage.sync.set({on: !data.on});
-
-      chrome.tabs.query({url: "https://web.whatsapp.com/"}, function(tabs) {
-        if (tabs.length !== 0)
-          tabs.forEach(function(tab){chrome.tabs.executeScript(tab.id, {file: '/load.js'})});
-      });
+chrome.commands.onCommand.addListener((command) => {
+  if(command == "toggle"){
+    chrome.storage.sync.get(['settings']).then((result) => {
+      if (result != null) {
+        result.settings.on = !result.settings.on;
+        chrome.storage.sync.set(result);
+      }
     });
   }
 });
 
-//set icon depending on extension status
-function toggleIcon(status){
-  chrome.browserAction.setIcon({path:'images/status' + (status == true ? 'On' : 'Off') + '.png'});
-}
-
-chrome.storage.onChanged.addListener(function(changes, area) {
-    if (area == "sync" && "on" in changes) {
-      toggleIcon(changes.on.newValue);
-    }
-});
-
-chrome.storage.sync.get(['on'], function(data) {
-  toggleIcon(data.on);
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area == "sync" && changes.settings != null) {
+    chrome.action.setIcon({
+      path: "images/status" + (changes.settings.newValue.on == true ? "On" : "Off") + ".png"
+    });
+  }
 });
