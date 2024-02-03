86e0a844f96d8773f2fc9752f1b4dbe51a87ad9c
Lukas <24789557+LukasLen@users.noreply.github.com>
2024-02-03 10:05:31 +0100

    Firefox support & code clean-up
    using browser namespace instead of chrome to support Firefox

--- src/background.js
+++ src/background.js
@@ -2,45 +2,58 @@
 /* Copyright (c) 2024 Lukas Lenhardt - lukaslen.com             */
 /* Released under the MIT license, see LICENSE file for details */
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get(["settings"]).then((result) => {
-    if (!result.hasOwnProperty("settings")) {
-      chrome.storage.sync.set({
-        settings: {
-          on: true,
-          currentPopupMessage: "",
-          styles: {
-            mediaGallery: true,
-            mediaPreview: true,
-            messages: true,
-            messagesPreview: true,
-            name: false,
-            noDelay: false,
-            profilePic: false,
-            textInput: true,
-            unblurActive: false
-          }
-        },
-      });
+// Remove this upon Chrome supporting the browser namespace
+if (typeof browser == "undefined") {
+  // Redefine browser namespace for Chrome for interoperability with Firefox
+  globalThis.browser = chrome;
+}
+
+const styleIdentifier = "pfwa";
+const settingsIdentifier = "settings";
+const defaultSettings =
+  {
+    settings: {
+      on: true,
+      currentPopupMessage: "",
+      styles: {
+        mediaGallery: true,
+        mediaPreview: true,
+        messages: true,
+        messagesPreview: true,
+        name: false,
+        noDelay: false,
+        profilePic: false,
+        textInput: true,
+        unblurActive: false
+      }
     }
+  };
+
+// Set default settings upon install
+browser.runtime.onInstalled.addListener(() => {
+  browser.storage.sync.get([settingsIdentifier]).then((result) => {
+    if (result.hasOwnProperty(settingsIdentifier)) return;
+    browser.storage.sync.set(defaultSettings);
   });
 });
 
-chrome.commands.onCommand.addListener((command) => {
-  if(command == "toggle"){
-    chrome.storage.sync.get(['settings']).then((result) => {
-      if (result != null) {
-        result.settings.on = !result.settings.on;
-        chrome.storage.sync.set(result);
-      }
-    });
-  }
+// Handle toggle command
+browser.commands.onCommand.addListener((command) => {
+  if (command != "toggle") return;
+
+  browser.storage.sync.get([settingsIdentifier]).then((result) => {
+    if (!result.hasOwnProperty(settingsIdentifier)) return;
+
+    result.settings.on = !result.settings.on;
+    browser.storage.sync.set(result);
+  });
 });
 
-chrome.storage.onChanged.addListener((changes, area) => {
-  if (area == "sync" && changes.settings != null) {
-    chrome.action.setIcon({
-      path: "images/status" + (changes.settings.newValue.on == true ? "On" : "Off") + ".png"
-    });
-  }
+// Update icon on setting change
+browser.storage.onChanged.addListener((changes, area) => {
+  if (area != "sync" || changes.settings == null) return;
+
+  browser.action.setIcon({
+    path: "images/status" + (changes.settings.newValue.on == true ? "On" : "Off") + ".png"
+  });
 });
--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -2,47 +2,64 @@
 /* Copyright (c) 2024 Lukas Lenhardt - lukaslen.com             */
 /* Released under the MIT license, see LICENSE file for details */
 
+// Remove this upon Chrome supporting the browser namespace
+if (typeof browser == "undefined") {
+  // Redefine browser namespace for Chrome for interoperability with Firefox
+  globalThis.browser = chrome;
+}
+
+const styleIdentifier = "pfwa";
+const settingsIdentifier = "settings";
+
 function removeStyleById(id) {
   if(el=document.getElementById(id)){
     el.parentNode.removeChild(el);
   }
 }
 
-function addStyleById(id) {
-  console.log(id);
-  if(!document.getElementById(id)){
-    var link = document.createElement('link');
-    link.id = id;
-    link.className = 'pfwa';
-    link.href = chrome.runtime.getURL('css/'+id+'.css');
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    document.getElementsByTagName("head")[0].appendChild(link);
+function removeAllStyles() {
+  var el = document.getElementsByClassName(styleIdentifier);
+  while (el.length > 0) {
+    el[0].parentNode.removeChild(el[0]);
   }
 }
 
-function updateStyles(settings) {
-  chrome.storage.sync.get(["settings"]).then((result) => {
-    if (result != null && result.settings.on) {
-      var styles = Object.keys(result.settings.styles);
-      styles.forEach((style) => {
-        if (result.settings.styles[style]) {
-          addStyleById(style);
-        } else {
-          removeStyleById(style);
-        }
-      });
-    } else {
-      var el = document.getElementsByClassName('pfwa');
-      while (el.length > 0) {
-        el[0].parentNode.removeChild(el[0]);
-      }
+function addStyleById(id) {
+  if (document.getElementById(id)) return;
+
+  var link = document.createElement('link');
+  link.id = id;
+  link.className = styleIdentifier;
+  link.href = browser.runtime.getURL('css/'+id+'.css');
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+function updateStyles() {
+  browser.storage.sync.get([settingsIdentifier]).then((result) => {
+    if (result == null || !result.settings.on) {
+      removeAllStyles();
+      return;
     }
+    
+    var styles = Object.keys(result.settings.styles);
+    styles.forEach((style) => {
+      if (result.settings.styles[style]) {
+        addStyleById(style);
+      } else {
+        removeStyleById(style);
+      }
+    });
   });
 }
 
-chrome.storage.onChanged.addListener((changes, area) => {
+// Update styles on setting change
+browser.storage.onChanged.addListener((changes, area) => {
   if (area == "sync" && changes.settings != null) {
     updateStyles();
   }
 });
+
+// Initial update once page loaded
+updateStyles();
